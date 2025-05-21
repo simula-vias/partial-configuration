- Make prepare_perf_matrix central function - Refactoring and bug fixes for wcp calculation

src/odt_labelled.py
@@ -1,4 +1,3 @@
-# %%
 import copy
 import json
 import multiprocessing
@@ -9,151 +8,14 @@ from functools import partial
 from pathlib import Path
 
 import numpy as np
-import pandas as pd
 from pydl85 import DL85Predictor
-from sklearn.preprocessing import KBinsDiscretizer, OneHotEncoder
 
-from common import NpEncoder, load_data
+from common import NpEncoder, load_data, prepare_perf_matrix
 
 
 def print_if_verbose(verbose, *args, **kwargs):
     if verbose:
         print(*args, **kwargs)
-
-
-# From odtlearn, installatin via pip failed somehow
-def binarize(
-    df, categorical_cols, integer_cols, real_cols, n_bins=4, bin_strategy="uniform"
-):
-    """
-    Parameters
-    ----------
-    df: pandas dataframe
-        A dataframe with only categorical/integer columns. There should not be any NA values.
-    categorical_cols: list
-        A list consisting of the names of categorical columns of df
-    integer_cols: list
-        A list consisting of the names of integer columns of df
-    real_cols: list
-        A list consisting of the names of real columns of df
-    n_bins: int
-        The number of bins to be used for encoding real columns
-    bin_strategy: str
-        The strategy to be used for discretizing real columns. It can be one of the following:
-        'uniform': All bins in each feature have identical widths.
-        'quantile': All bins in each feature have the same number of points.
-
-    Return
-    ----------
-    the binarized version of the input dataframe.
-
-    This function encodes each categorical column as a one-hot vector, i.e.,
-    for each level of the feature, it creates a new binary column with a value
-    of one if and only if the original column has the corresponding level.
-    A similar approach for encoding integer features is used with a slight change.
-    The new binary column should have a value of one if and only if the main column
-    has the corresponding value or any value smaller than it.
-    We first discretize the real columns according to the number of bins and the discretization strategy
-    and then treat them as integer columns.
-    """
-
-    assert len(categorical_cols) > 0 or len(integer_cols) > 0 or len(real_cols) > 0, (
-        "Must provide at least one of the three options of a list of categorical columns "
-        "or integer columns or real valued columns to binarize."
-    )
-
-    if len(real_cols) > 0 and n_bins is None:
-        raise ValueError(
-            "The number of bins must be provided for encoding real columns."
-        )
-    if len(real_cols) > 0 and bin_strategy is None:
-        raise ValueError("The bin strategy must be provided for encoding real columns.")
-    if (
-        len(real_cols) > 0
-        and bin_strategy is None
-        or bin_strategy not in ["uniform", "quantile"]
-    ):
-        raise ValueError(
-            "The bin strategy must be one of the following: 'uniform' or 'quantile'."
-        )
-
-    if len(categorical_cols) > 0:
-        X_cat = np.array(df[categorical_cols])
-        enc = OneHotEncoder(handle_unknown="error", drop="if_binary")
-        X_cat_enc = enc.fit_transform(X_cat).toarray()
-        categorical_cols_enc = enc.get_feature_names_out(categorical_cols)
-        X_cat_enc = X_cat_enc.astype(int)
-    if len(real_cols) > 0:
-        # We first bucketize the real columns according to number of bins and the
-        # discretization strategy and then treat them as integer columns
-        discretizer_unif = KBinsDiscretizer(
-            n_bins=n_bins, encode="ordinal", strategy=bin_strategy
-        )
-        df[real_cols] = discretizer_unif.fit_transform(df[real_cols])
-        integer_cols = integer_cols + real_cols
-    if len(integer_cols) > 0:
-        X_int = np.array(df[integer_cols])
-        enc = OneHotEncoder(handle_unknown="error", drop="if_binary")
-        X_int_enc = enc.fit_transform(X_int).toarray()
-        integer_cols_enc = enc.get_feature_names_out(integer_cols)
-        X_int_enc = X_int_enc.astype(int)
-
-        for col in integer_cols:
-            col_enc_set = []
-            col_offset = None
-            for i, col_enc in enumerate(integer_cols_enc):
-                if col in col_enc:
-                    col_enc_set.append(col_enc)
-                    if col_offset is None:
-                        col_offset = i
-            if len(col_enc_set) < 3:
-                continue
-            for i, col_enc in enumerate(col_enc_set):
-                if i == 0:
-                    continue
-                X_int_enc[:, (col_offset + i)] = (
-                    X_int_enc[:, (col_offset + i)] | X_int_enc[:, (col_offset + i - 1)]
-                )
-    if len(categorical_cols) > 0 and len(integer_cols) > 0:
-        df_enc = pd.DataFrame(
-            np.c_[X_cat_enc, X_int_enc],
-            columns=list(categorical_cols_enc) + list(integer_cols_enc),
-        )
-    elif len(categorical_cols) > 0 and len(integer_cols) == 0:
-        df_enc = pd.DataFrame(
-            X_cat_enc,
-            columns=list(categorical_cols_enc),
-        )
-    elif len(categorical_cols) == 0 and len(integer_cols) > 0:
-        df_enc = pd.DataFrame(
-            X_int_enc,
-            columns=list(integer_cols_enc),
-        )
-    return df_enc
-
-
-def prepare_perf_matrx(perf_matrix_initial, performances):
-    nmdf = (
-        perf_matrix_initial[["inputname"] + performances]
-        .groupby("inputname", as_index=True)
-        # .transform(lambda x: scale(x))
-        .transform(lambda x: (x - x.min()) / (x.max() - x.min()))
-    )
-    nmdf["worst_case_performance"] = nmdf[performances].max(axis=1)
-    perf_matrix = pd.merge(
-        perf_matrix_initial,
-        nmdf,
-        suffixes=("_raw", None),
-        left_index=True,
-        right_index=True,
-    )
-    # We adjust the WCP by expressing it as the difference from the best WCP, i.e. the best WCP is always 0
-    perf_matrix["worst_case_performance"] = (
-        perf_matrix[["inputname", "worst_case_performance"]]
-        .groupby("inputname", as_index=True)
-        .transform(lambda x: x - x.min())
-    )
-    return perf_matrix
 
 
 def evaluate_wcp_mean(y_pred, inputs, C):
@@ -222,14 +84,15 @@ def evaluate_result(
     res,
     splits,
     input_feature_columns,
+    input_preprocessor,
     max_depth=None,
     verbose=True,
 ):
     num_configs = res["num_configs"]
     system = res["system"]
     performances = res["performances"]
-    min_wcp_mean = res["wcp_mean"]
-    min_wcp_max = res["wcp_max"]
+    min_wcp_mean_full = res["wcp_mean"]
+    min_wcp_max_full = res["wcp_max"]
     inp_cfg_map = res["input_to_config_map"]
     selected_configs = res["selected_configs"]
 
@@ -237,10 +100,10 @@ def evaluate_result(
     print_if_verbose(verbose, f"System: {system}")
     print_if_verbose(verbose, f"Num. configs: {num_configs}")
     print_if_verbose(verbose, f"Performances: {performances}")
-    print_if_verbose(verbose, f"wcp_mean: {min_wcp_mean:.4f}")
-    print_if_verbose(verbose, f"wcp_max: {min_wcp_max:.4f}")
+    print_if_verbose(verbose, f"wcp_mean: {min_wcp_mean_full:.4f}")
+    print_if_verbose(verbose, f"wcp_max: {min_wcp_max_full:.4f}")
 
-    perf_matrix = prepare_perf_matrx(perf_matrix_initial, performances)
+    perf_matrix = prepare_perf_matrix(perf_matrix_initial, performances)
 
     feature_matrix = perf_matrix[
         list(input_feature_columns) + ["inputname"]
@@ -250,7 +113,7 @@ def evaluate_result(
 
     # Transform features
     X_all = input_preprocessor.fit_transform(features)
-    feature_names = input_preprocessor.get_feature_names_out()
+    # feature_names = input_preprocessor.get_feature_names_out()
 
     if max_depth is None:
         max_depth = X_all.shape[1]
@@ -272,17 +135,38 @@ def evaluate_result(
     # This is the label assignment from the OCS results
     y_all = np.array([inp_cfg_map[inp] for inp in inputnames])
 
+    wcp_mean_best = evaluate_wcp_mean(y_all, inputnames, C)
+    wcp_max_best = evaluate_wcp_max(y_all, inputnames, C)
+    assert np.isclose(wcp_mean_best, min_wcp_mean_full), (
+        f"{wcp_mean_best=}, {min_wcp_mean_full=} {num_configs=} {performances=}"
+    )
+    assert np.isclose(wcp_max_best, min_wcp_max_full), (
+        f"{wcp_max_best=}, {min_wcp_max_full=}"
+    )
+
     # Check if they agree
     label_agreement = (y_argmin == y_all).mean()
     print_if_verbose(verbose, f"label agreement: {label_agreement:.2f}")
 
     def evaluate(
-        clf, train_inp, X_train, y_train, test_inp=None, X_test=None, y_test=None
+        clf,
+        train_inp,
+        X_train,
+        y_train,
+        test_inp=None,
+        X_test=None,
+        y_test=None,
+        train_wcp_mean_min=None,
+        train_wcp_max_min=None,
+        test_wcp_mean_min=None,
+        test_wcp_max_min=None,
     ):
         y_pred = clf.predict(X_train)
         train_acc = np.sum(y_pred == y_train) / y_train.shape[0]
         train_wcp_mean = evaluate_wcp_mean(y_pred, train_inp, C)
         train_wcp_max = evaluate_wcp_max(y_pred, train_inp, C)
+        train_wcp_mean_gap = train_wcp_mean - train_wcp_mean_min
+        train_wcp_max_gap = train_wcp_max - train_wcp_max_min
         num_classes_used = len(np.unique(y_pred))
 
         if test_inp is not None and X_test is not None and y_test is not None:
@@ -290,8 +174,8 @@ def evaluate_result(
             test_acc = np.sum(test_pred == y_test) / y_test.shape[0]
             test_wcp_mean = evaluate_wcp_mean(test_pred, test_inp, C)
             test_wcp_max = evaluate_wcp_max(test_pred, test_inp, C)
-            test_wcp_mean_gap = test_wcp_mean - min_wcp_mean
-            test_wcp_max_gap = test_wcp_max - min_wcp_max
+            test_wcp_mean_gap = test_wcp_mean - test_wcp_mean_min
+            test_wcp_max_gap = test_wcp_max - test_wcp_max_min
         else:
             test_pred = None
             test_acc = None
@@ -310,8 +194,8 @@ def evaluate_result(
             "test_wcp_max": test_wcp_max,
             "train_pred": y_pred,
             "test_pred": test_pred,
-            "train_wcp_mean_gap": train_wcp_mean - min_wcp_mean,
-            "train_wcp_max_gap": train_wcp_max - min_wcp_max,
+            "train_wcp_mean_gap": train_wcp_mean_gap,
+            "train_wcp_max_gap": train_wcp_max_gap,
             "test_wcp_mean_gap": test_wcp_mean_gap,
             "test_wcp_max_gap": test_wcp_max_gap,
         }
@@ -321,8 +205,14 @@ def evaluate_result(
     print_if_verbose(verbose, "Training DL8.5 on full dataset")
     for d in range(1, max_depth + 1):
         clf = train_dl85(X_train=X_all, y_train=y_all, max_depth=d)
-        print_if_verbose(verbose, "lattice size", clf.lattice_size_)
-        eval_result = evaluate(clf, train_inp=inputnames, X_train=X_all, y_train=y_all)
+        eval_result = evaluate(
+            clf,
+            train_inp=inputnames,
+            X_train=X_all,
+            y_train=y_all,
+            train_wcp_mean_min=min_wcp_mean_full,
+            train_wcp_max_min=min_wcp_max_full,
+        )
         results.append(
             {
                 "model": "dl85",
@@ -367,6 +257,11 @@ def evaluate_result(
         y_train = np.array([inp_cfg_map[inp] for inp in train_inp])
         y_test = np.array([inp_cfg_map[inp] for inp in test_inp])
 
+        min_wcp_mean_split_train = evaluate_wcp_mean(y_train, train_inp, C)
+        min_wcp_max_split_train = evaluate_wcp_max(y_train, train_inp, C)
+        min_wcp_mean_split_test = evaluate_wcp_mean(y_test, test_inp, C)
+        min_wcp_max_split_test = evaluate_wcp_max(y_test, test_inp, C)
+
         for d in range(1, max_depth + 1):
             clf = train_dl85(X_train=X_train, y_train=y_train, max_depth=d)
             eval_result = evaluate(
@@ -377,6 +272,10 @@ def evaluate_result(
                 test_inp=test_inp,
                 X_test=X_test,
                 y_test=y_test,
+                train_wcp_mean_min=min_wcp_mean_split_train,
+                train_wcp_max_min=min_wcp_max_split_train,
+                test_wcp_mean_min=min_wcp_mean_split_test,
+                test_wcp_max_min=min_wcp_max_split_test,
             )
             results.append(
                 {
@@ -407,7 +306,7 @@ def evaluate_result(
     return results
 
 
-def worker_function(res_item, perf_matrix, input_cols, splits_data):
+def worker_function(res_item, perf_matrix, input_cols, splits_data, input_preprocessor):
     """
     Wrapper function to be called by each process.
     It calls the original evaluate_result.
@@ -421,18 +320,17 @@ def worker_function(res_item, perf_matrix, input_cols, splits_data):
         res=res_item,
         input_feature_columns=input_cols,
         splits=splits_data,
+        input_preprocessor=input_preprocessor,
         verbose=False,
     )
     return result_list
 
 
-# %%
-
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--system", type=str, default="xz")
+    parser.add_argument("--system", type=str, default="gcc")
     parser.add_argument("--data_dir", type=str, default="./data")
     parser.add_argument("--result_dir", type=str, default="./results")
     parser.add_argument("--processes", type=int, default=1)
@@ -441,10 +339,10 @@ if __name__ == "__main__":
     (
         perf_matrix_initial,
         input_features,
-        config_features,
-        all_performances,
+        _,
+        _,
         input_preprocessor,
-        config_preprocessor,
+        _,
     ) = load_data(
         system=args.system, data_dir=args.data_dir, input_properties_type="tabular"
     )
@@ -472,6 +370,7 @@ if __name__ == "__main__":
             perf_matrix=perf_matrix_initial,
             input_cols=input_features.columns,
             splits_data=splits,
+            input_preprocessor=input_preprocessor,
         )
 
         start_time = time.time()
@@ -514,6 +413,7 @@ if __name__ == "__main__":
                     res=res,
                     input_feature_columns=input_features.columns,
                     splits=splits,
+                    input_preprocessor=input_preprocessor,
                 )
             )
             json.dump(
@@ -524,5 +424,3 @@ if __name__ == "__main__":
             )
 
     print("Done.")
-
-# %%